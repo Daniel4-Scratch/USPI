82862cc9999d639553c1e8191c417efa03522d67
Daniel Barton <mineminecraftpe687@gmail.com>
2020-12-31 15:16:12 +1100

    Create UserScript-Starter.js

---
+++ UserScript-Starter.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         My First UserScript App
+// @namespace    https://example.com
+// @version      1.0
+// @description  My very first UserScript application
+// @author       John Doe
+// @match        *https://example.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    alert("Hello!")
+})();
